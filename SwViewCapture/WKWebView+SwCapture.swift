25353e7ef3460b34604f764c6ef6f6274422bffd
chenxing <chenxing@didichuxing.com>
2016-02-26 15:13:33 +0800

    optimize wkwebview shot implement

--- SwViewCapture/WKWebView+SwCapture.swift
+++ SwViewCapture/WKWebView+SwCapture.swift
@@ -47,8 +47,6 @@ public extension WKWebView {
     private func swContentCaptureWithoutOffset(completionHandler:(capturedImage: UIImage?) -> Void) {
         let containerView  = UIView(frame: self.bounds)
         
-        let lastOffset = self.offsetForLastView()
-        
         let bakFrame     = self.frame
         let bakSuperView = self.superview
         let bakIndex     = self.superview?.subviews.indexOf(self)
@@ -61,10 +59,12 @@ public extension WKWebView {
         
         // Divide
         let page       = floorf(Float( totalSize.height / containerView.bounds.height))
+        
+        self.frame = CGRectMake(0, 0, containerView.bounds.size.width, self.scrollView.contentSize.height)
 
         UIGraphicsBeginImageContextWithOptions(totalSize, false, UIScreen.mainScreen().scale)
         
-        self.swContentPageDraw(containerView, index: 0, maxIndex: Int(page), lastOffset: lastOffset, drawCallback: { [weak self] () -> Void in
+        self.swContentPageDraw(containerView, index: 0, maxIndex: Int(page), drawCallback: { [weak self] () -> Void in
             let strongSelf = self!
             
             let capturedImage = UIGraphicsGetImageFromCurrentImageContext()
@@ -82,22 +82,20 @@ public extension WKWebView {
         })
     }
     
-    private func swContentPageDraw (targetView: UIView, index: Int, maxIndex: Int, lastOffset: CGFloat, drawCallback: () -> Void) {
+    private func swContentPageDraw (targetView: UIView, index: Int, maxIndex: Int, drawCallback: () -> Void) {
         
         // set up split frame of super view
-        var splitFrame = CGRectMake(0, CGFloat(index) * targetView.frame.size.height, targetView.bounds.size.width, targetView.frame.size.height)
+        let splitFrame = CGRectMake(0, CGFloat(index) * targetView.frame.size.height, targetView.bounds.size.width, targetView.frame.size.height)
         // set up webview frame
-        self.frame = CGRectMake(0, -(CGFloat(index) * targetView.frame.size.height), targetView.bounds.size.width, (CGFloat(index + 1) * targetView.frame.size.height))
-        
-        if (index == maxIndex) {
-            splitFrame = CGRectMake(0, CGFloat(index) * targetView.frame.size.height - lastOffset, targetView.bounds.size.width,  targetView.frame.size.height)
-        }
+        var myFrame = self.frame
+        myFrame.origin.y = -(CGFloat(index) * targetView.frame.size.height)
+        self.frame = myFrame
         
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(0.3 * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) { () -> Void in
             targetView.drawViewHierarchyInRect(splitFrame, afterScreenUpdates: true)
             
             if index < maxIndex {
-                self.swContentPageDraw(targetView, index: index + 1, maxIndex: maxIndex, lastOffset: lastOffset, drawCallback: drawCallback)
+                self.swContentPageDraw(targetView, index: index + 1, maxIndex: maxIndex, drawCallback: drawCallback)
             }else{
                 drawCallback()
             }
@@ -156,34 +154,4 @@ public extension WKWebView {
             }
         }
     }
-    
-    // MARK: Util Methods
-    // WKWebview maybe show a offset at last page view capture on UINavigationController.
-    // case 1. view on presentedViewController
-    // case 2. view on UINavigationController (UINavigationBar is Showing)
-    // case 3. view on UINavigationController (UINavigationBar is Hidding)
-    private func foundViewControllerByView(view: UIView) -> UIViewController? {
-        var responder = self.nextResponder();
-        while responder !=  nil {
-            if responder!.isKindOfClass(UIViewController) {
-                return responder as? UIViewController
-            }
-            responder = responder!.nextResponder()
-        }
-        return nil
-    }
-    
-    private func offsetForLastView() -> CGFloat {
-        let vc = self.foundViewControllerByView(self)
-        if ((vc?.navigationController) != nil){
-            if ((vc?.navigationController?.navigationBarHidden) == true) {
-                return 20
-            }else{
-                return 64
-            }
-        }
-        
-        return 0
-    }
-    
 }
