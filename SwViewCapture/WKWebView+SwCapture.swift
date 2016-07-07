6bc56c317432ea23e1c3faeb8c03b299722b90f3
chenxing <chenxing@didichuxing.com>
2016-07-07 11:46:26 +0800

    fix crash when capture failed problem

--- SwViewCapture/WKWebView+SwCapture.swift
+++ SwViewCapture/WKWebView+SwCapture.swift
@@ -39,7 +39,7 @@ public extension WKWebView {
                 
                 strongSelf.isCapturing = false
                 
-                completionHandler(capturedImage: capturedImage!)
+                completionHandler(capturedImage: capturedImage)
             })
         }
     }
