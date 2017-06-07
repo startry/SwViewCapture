6ca724469eb76f15c1c782827eabc7bfeccf2ade
chenxing <chenxing@didichuxing.com>
2017-06-07 14:11:01 +0800

    Add UICollection to Demo Project

---
+++ Example/Example/STCollectionViewDemoController.swift
@@ -0,0 +1,72 @@
+//
+//  STCollectionViewDemoController.swift
+//  Example
+//
+//  Created by chenxing.cx on 2017/6/7.
+//  Copyright © 2017年 Startry. All rights reserved.
+//
+
+import UIKit
+
+class STCollectionViewDemoController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
+
+    let cellIdentify = "reuseableCellIdentify"
+    var collectView : UICollectionView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Capture", style: UIBarButtonItemStyle.plain, target: self, action: #selector(STTableViewDemoController.didCaptureBtnClicked(_:)))
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = UICollectionViewScrollDirection.vertical;
+        collectView = UICollectionView(frame: CGRect.zero, collectionViewLayout: flowLayout)
+        
+        collectView?.dataSource = self
+        collectView?.delegate   = self
+        
+        collectView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellIdentify)
+        
+        view.addSubview(collectView!)
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectView?.frame = view.bounds
+    }
+    
+    // MARK: UICollectionView DataSource & Delegate
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 500;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectView?.dequeueReusableCell(withReuseIdentifier: cellIdentify, for: indexPath);
+        
+        if indexPath.row % 2 == 1 {
+            cell?.contentView.backgroundColor = UIColor.orange
+        }else {
+            cell?.contentView.backgroundColor = UIColor.yellow
+        }
+        
+        return cell!;
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1;
+    }
+
+    // MARK : Capture Button Events
+    func didCaptureBtnClicked(_ button: UIButton){
+        
+        collectView?.swContentCapture({ (capturedImage) -> Void in
+            let vc = ImageViewController(image: capturedImage!)
+            self.navigationController?.pushViewController(vc, animated: true)
+        })
+        
+        //        tableView?.swContentScrollCapture({ (capturedImage) -> Void in
+        //            let vc = ImageViewController(image: capturedImage!)
+        //            self.navigationController?.pushViewController(vc, animated: true)
+        //        })
+    }
+}
--- Example/Example/STMenuDemoViewController.swift
+++ Example/Example/STMenuDemoViewController.swift
@@ -15,6 +15,7 @@ class STMenuDemoViewController: UIViewController {
     fileprivate var tableViewBtn: UIButton?
     fileprivate var webViewBtn: UIButton?
     fileprivate var oldWebViewBtn: UIButton?
+    fileprivate var collectionBtn: UIButton?
     
 // MARK: Life Cycle
     
@@ -39,24 +40,28 @@ class STMenuDemoViewController: UIViewController {
         let tableViewBtn  = UIButton()
         let webViewBtn    = UIButton()
         let oldWebViewBtn = UIButton()
+        let collectBtn    = UIButton()
         
-        viewBtn.setTitleColor(UIColor.black, for: UIControlState())
-        scrollViewBtn.setTitleColor(UIColor.black, for: UIControlState())
-        tableViewBtn.setTitleColor(UIColor.black, for: UIControlState())
-        webViewBtn.setTitleColor(UIColor.black, for: UIControlState())
-        oldWebViewBtn.setTitleColor(UIColor.black, for: UIControlState())
+        viewBtn.setTitleColor(UIColor.black, for: UIControlState.normal)
+        scrollViewBtn.setTitleColor(UIColor.black, for: UIControlState.normal)
+        tableViewBtn.setTitleColor(UIColor.black, for: UIControlState.normal)
+        webViewBtn.setTitleColor(UIColor.black, for: UIControlState.normal)
+        oldWebViewBtn.setTitleColor(UIColor.black, for: UIControlState.normal)
+        collectBtn.setTitleColor(UIColor.black, for: UIControlState.normal)
         
-        viewBtn.setTitle("View示例", for: UIControlState())
-        scrollViewBtn.setTitle("ScrollView示例", for: UIControlState())
-        tableViewBtn.setTitle("TableView示例", for: UIControlState())
-        webViewBtn.setTitle("WKWebView示例", for: UIControlState())
-        oldWebViewBtn.setTitle("UIWebView示例", for: UIControlState())
+        viewBtn.setTitle("View示例", for: UIControlState.normal)
+        scrollViewBtn.setTitle("ScrollView示例", for: UIControlState.normal)
+        tableViewBtn.setTitle("TableView示例", for: UIControlState.normal)
+        webViewBtn.setTitle("WKWebView示例", for: UIControlState.normal)
+        oldWebViewBtn.setTitle("UIWebView示例", for: UIControlState.normal)
+        collectBtn.setTitle("CollectionView示例", for: UIControlState.normal)
         
         view.addSubview(viewBtn)
         view.addSubview(scrollViewBtn)
         view.addSubview(tableViewBtn)
         view.addSubview(webViewBtn)
         view.addSubview(oldWebViewBtn)
+        view.addSubview(collectBtn)
         
         let actionSel = #selector(STMenuDemoViewController.didBtnClicked(_:))
         
@@ -65,12 +70,14 @@ class STMenuDemoViewController: UIViewController {
         tableViewBtn.addTarget(self, action: actionSel, for: UIControlEvents.touchUpInside)
         webViewBtn.addTarget(self, action: actionSel, for: UIControlEvents.touchUpInside)
         oldWebViewBtn.addTarget(self, action: actionSel, for: UIControlEvents.touchUpInside)
+        collectBtn.addTarget(self, action: actionSel, for: UIControlEvents.touchUpInside)
         
         self.viewBtn = viewBtn
         self.scrollViewBtn = scrollViewBtn
         self.tableViewBtn = tableViewBtn
         self.webViewBtn = webViewBtn
         self.oldWebViewBtn = oldWebViewBtn
+        self.collectionBtn = collectBtn;
     }
     
 // MARK: Layout Views
@@ -81,11 +88,14 @@ class STMenuDemoViewController: UIViewController {
         let btnHeight:CGFloat = 30, btnWidth:CGFloat = 180.0, spanHeight:CGFloat = 20.0;
         let btnX = view.frame.size.width / 2 - btnWidth / 2;
         let midY = view.frame.size.height / 2;
-        viewBtn?.frame = CGRect(x: btnX, y: midY - 2 * spanHeight - btnHeight * 2.5, width: btnWidth, height: btnHeight)
-        scrollViewBtn?.frame = CGRect(x: btnX, y: midY - spanHeight - btnHeight * 1.5, width: btnWidth, height: btnHeight)
-        tableViewBtn?.frame = CGRect(x: btnX, y: midY - btnHeight * 0.5, width: btnWidth, height: btnHeight)
-        webViewBtn?.frame = CGRect(x: btnX, y: midY + spanHeight + btnHeight * 0.5, width: btnWidth, height: btnHeight)
-        oldWebViewBtn?.frame = CGRect(x: btnX, y: midY + spanHeight * 2 + btnHeight * 1.5, width: btnWidth, height: btnHeight)
+        
+        viewBtn?.frame       = CGRect(x: btnX, y: midY - btnHeight * 3 - spanHeight * 2.5, width: btnWidth, height: btnHeight)
+        scrollViewBtn?.frame = CGRect(x: btnX, y: midY - btnHeight * 2 - spanHeight * 1.5, width: btnWidth, height: btnHeight)
+        tableViewBtn?.frame  = CGRect(x: btnX, y: midY - btnHeight - spanHeight / 2, width: btnWidth, height: btnHeight)
+        
+        webViewBtn?.frame    = CGRect(x: btnX, y: midY + spanHeight * 0.5, width: btnWidth, height: btnHeight)
+        oldWebViewBtn?.frame = CGRect(x: btnX, y: midY + spanHeight * 1.5 + btnHeight, width: btnWidth, height: btnHeight)
+        collectionBtn?.frame = CGRect(x: btnX, y: midY + spanHeight * 2.5 + btnHeight * 2, width: btnWidth, height: btnHeight)
     }
     
 // MARK: Events
@@ -106,6 +116,9 @@ class STMenuDemoViewController: UIViewController {
             navigationController?.pushViewController(vc, animated: true)
         }else if(button == oldWebViewBtn) {
             let vc = STUIWebViewDemoController()
+            navigationController?.pushViewController(vc, animated: true)
+        }else if(button == collectionBtn) {
+            let vc = STCollectionViewDemoController()
             navigationController?.pushViewController(vc, animated: true)
         }
     }
